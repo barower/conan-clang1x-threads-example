8fe92c6593f01ca6664a2714c8eeb3f4476db3dd
Bartłomiej Osiński <bartlomiej.osinski@enigma.com.pl>
2022-02-03 16:28:15 +0100

    Init

---
+++ build_clang11.sh
@@ -0,0 +1,6 @@
+COMMAND="cd /home/project && mkdir build && cd build && conan install ../ && conan build ../"
+
+docker run -it \
+	--mount type=bind,source=`realpath ./`,target=/home/project \
+	conanio/clang11 \
+	/bin/bash -c "$COMMAND"
